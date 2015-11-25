4bf7858ccd50d6b7d178aa8589668cb24f8198db
Christopher Su <christophersu9@gmail.com>
2015-11-24 21:15:39 -0800

    working

--- twitbot_img.py
+++ twitbot_img.py
@@ -1,1 +1,19 @@
+import os
+
 import config
+
+def process_tweet(tweet_id, tweet_text, mentions, api):
+    tweet_img = None
+    for image, keywords in config.images.iteritems():
+        for keyword in keywords:
+            # this is highly inefficient
+            if keyword in tweet_text:
+                tweet_img = image
+                break
+
+    if tweet_img:
+        tweet_img = os.path.join(config.image_folder, tweet_img)
+        content = '%s %s' % (' '.join(mentions), tweet_text)
+        api.update_with_media(tweet_img,
+            status=content,
+            in_reply_to_status_id=tweet_id)
